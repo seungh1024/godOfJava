30e80de89148aded4b52f6695fbf8c9aa1014d80
seungh <seungh1024@naver.com>
2023-12-05 03:27:25 +0900

    [ADD] network socket practice

--- src/main/java/com/seungh1024/network/SocketClientSample.java
+++ src/main/java/com/seungh1024/network/SocketClientSample.java
@@ -1,7 +1,6 @@
 package com.seungh1024.network;
 
-import java.io.BufferedOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.util.Date;
 
@@ -11,14 +10,16 @@ public class SocketClientSample {
         sample.sendSocketSample();
     }
 
-    private void sendSocketSample() {
+    public void sendSocketSample() {
         for (int i = 0; i < 3; i++) {
-            sendSocketData("I liked java at " + new Date());
+//            sendSocketData("I liked java at " + new Date());
+            sendAndReceiveSocketData("I liked java at " + new Date());
         }
-        sendSocketData("EXIT");
+//        sendSocketData("EXIT");
+        sendAndReceiveSocketData("EXIT");
     }
 
-    private void sendSocketData(String data) {
+    public void sendSocketData(String data) {
         Socket socket = null;
         try {
             System.out.println("Client: Connecting");
@@ -30,7 +31,56 @@ public class SocketClientSample {
             byte[] bytes = data.getBytes();
             out.write(bytes);
             System.out.println("Client: Sent data = " + data);
+
             out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void sendAndReceiveSocketData(String data) {
+        Socket socket = null;
+        try {
+            System.out.println("Client: Connecting");
+            socket = new Socket("127.0.0.1", 9999);
+            System.out.println("Client: Connect status = " + socket.isConnected());
+            Thread.sleep(2000);
+            OutputStream stream = socket.getOutputStream();
+            BufferedOutputStream out = new BufferedOutputStream(stream);
+            data +="\n";
+            byte[] bytes = data.getBytes();
+            out.write(bytes);
+            out.flush();
+            out.write("stop\n".getBytes());
+            out.flush();
+
+
+
+            System.out.println("Client: Sent data = " + data);
+
+            System.out.println(socket.isConnected());
+
+
+            System.out.println("Client: receive from Server");
+            InputStream inputStream = socket.getInputStream();
+            BufferedInputStream in = new BufferedInputStream(inputStream);
+            byte[] readByte = new byte[256];
+            in.read(readByte);
+
+
+            System.out.println("Receive Response = " + new String(readByte).trim());
+            stream.close();
+            in.close();
+            inputStream.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }finally {
--- src/main/java/com/seungh1024/network/SocketServerSample.java
+++ src/main/java/com/seungh1024/network/SocketServerSample.java
@@ -1,18 +1,17 @@
 package com.seungh1024.network;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SocketServerSample {
     public static void main(String[] args) {
         SocketServerSample sample = new SocketServerSample();
-        sample.startServer();
+//        sample.startServer();
+        sample.startReplyServer();
     }
 
-    private void startServer() {
+    public void startServer() {
         ServerSocket server = null;
         Socket client = null;
         try {
@@ -29,6 +28,69 @@ public class SocketServerSample {
                     receivedData.append(data);
                 }
                 System.out.println("Received data: " + receivedData);
+
+                in.close();
+                stream.close();
+                client.close();
+
+                if (receivedData != null && "EXIT".equals(receivedData.toString())) {
+                    System.out.println("Stop SocketServer");
+                    break;
+                }
+                System.out.println("--------------");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (server != null) {
+                try {
+                    server.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void startReplyServer() {
+        ServerSocket server = null;
+        Socket client = null;
+        try {
+            server = new ServerSocket(9999);
+            while (true) {
+                System.out.println("Server:Waiting for request.");
+                client = server.accept();
+                System.out.println("Server: accepted");
+                InputStream stream = client.getInputStream();
+                BufferedReader in = new BufferedReader(new InputStreamReader(stream));
+
+
+                String data = null;
+                StringBuilder receivedData = new StringBuilder();
+                System.out.println("before while");
+
+                while (true) {
+                    data = in.readLine();
+                    System.out.println(data);
+                    if(data.equals("stop")) break;
+                    receivedData.append(data);
+                }
+                System.out.println("Received data: " + receivedData);
+
+
+                System.out.println("Send Response");
+                OutputStream outputStream = client.getOutputStream();
+                BufferedOutputStream out = new BufferedOutputStream(outputStream);
+                data = "OK";
+                byte[] buffer = data.getBytes();
+                System.out.println(client.isConnected());
+                out.write(buffer);
+                out.flush();
+                System.out.println("send Response Success");
+
+                Thread.sleep(1000);
+                out.close();
+                outputStream.close();
                 in.close();
                 stream.close();
                 client.close();
